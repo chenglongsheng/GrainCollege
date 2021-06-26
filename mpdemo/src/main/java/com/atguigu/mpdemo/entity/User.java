eeb05081470c34f7a830d5a0086980d3c6bdb0b0
cls688 <1536463948@qq.com>
2021-06-26 23:15:58 +0800

    实体类

---
+++ mpdemo/src/main/java/com/atguigu/mpdemo/entity/User.java
@@ -0,0 +1,15 @@
+package com.atguigu.mpdemo.entity;
+
+import lombok.Data;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-06-26 22:56
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}

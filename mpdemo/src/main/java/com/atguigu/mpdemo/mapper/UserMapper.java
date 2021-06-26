aec849df5b462d71984d96881197cd466af213d4
cls688 <1536463948@qq.com>
2021-06-26 23:17:36 +0800

    mapper接口

---
+++ mpdemo/src/main/java/com/atguigu/mpdemo/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.atguigu.mpdemo.mapper;
+
+import com.atguigu.mpdemo.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-06-26 22:59
+ */
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+}

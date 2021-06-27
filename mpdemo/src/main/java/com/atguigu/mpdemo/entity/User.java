6c363360833d56f2d47d29070f127112281f066d
cls688 <1536463948@qq.com>
2021-06-27 11:15:39 +0800

    mp自带的主键生成策略

--- mpdemo/src/main/java/com/atguigu/mpdemo/entity/User.java
+++ mpdemo/src/main/java/com/atguigu/mpdemo/entity/User.java
@@ -1,5 +1,7 @@
 package com.atguigu.mpdemo.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 /**
@@ -8,6 +10,19 @@ import lombok.Data;
  */
 @Data
 public class User {
+
+    /*
+    mp自带的主键生成策略
+    AUTO：自动增长
+
+    ID_WORKER：mp自带策略，生成19位值，数字类型使用这种策略，如long
+    ID_WORKER_STR：mp自带策略，生成19位值，字符串类型使用这种策略
+
+    INPUT：设置id值
+    NONE：无策略，输入
+    UUID：随机唯一值
+     */
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
     private Integer age;

2c403d12321f7fb10cae8e92245884b11d6dead8
cls688 <1536463948@qq.com>
2021-06-27 22:40:34 +0800

    转移配置的存放地方

--- mpdemo/src/main/java/com/atguigu/mpdemo/MpdemoApplication.java
+++ mpdemo/src/main/java/com/atguigu/mpdemo/MpdemoApplication.java
@@ -1,11 +1,9 @@
 package com.atguigu.mpdemo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.atguigu.mpdemo.mapper")
 public class MpdemoApplication {
 
     public static void main(String[] args) {

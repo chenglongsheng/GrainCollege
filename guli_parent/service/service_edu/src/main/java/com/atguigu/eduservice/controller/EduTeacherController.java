1c0aae977c53c18a9e02dadda4042e9dc8d7d362
cls688 <1536463948@qq.com>
2021-06-28 22:56:09 +0800

    查询所有讲师

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,14 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
+
+    //把service注入
+    @Autowired
+    private EduTeacherService teacherService;
+
+    //1.查询讲师表中所有数据
+    //rest风格
+    @GetMapping("/findAll")
+    public List<EduTeacher> findAllTeacher() {
+        final List<EduTeacher> list = teacherService.list(null);
+        return list;
+    }
 
 }
 

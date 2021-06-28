35fa973c80480a0b5f06d116b71ec58945d6ff8f
cls688 <1536463948@qq.com>
2021-06-28 22:55:00 +0800

    创建启动类

---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.eduservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-06-28 22:41
+ */
+@SpringBootApplication
+public class EduApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class, args);
+    }
+}

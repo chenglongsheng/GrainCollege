727963dc48c650c551a5972414dd0e7c2c11207e
cls688 <1536463948@qq.com>
2021-06-28 11:22:10 +0800

    配置SQL执行性能分析插件

--- mpdemo/src/main/java/com/atguigu/mpdemo/config/MpConfig.java
+++ mpdemo/src/main/java/com/atguigu/mpdemo/config/MpConfig.java
@@ -4,9 +4,11 @@ import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @author chenglongsheng
@@ -34,6 +36,20 @@ public class MpConfig {
     @Bean
     public ISqlInjector sqlInjector() {
         return new LogicSqlInjector();
+    }
+
+    /*
+    SQL执行性能分析插件
+    开发环境使用，线上不推荐。maxTime指SQL最大执行时长
+    环境：dev（开发）、test（测试）、prod（生产）
+     */
+    @Bean
+    @Profile({"dev", "test"})
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setMaxTime(100);// ms,超过时间SQL不执行
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
     }
 
 }

450bc5d78d795566395dcc72f0846634b940ea89
cls688 <1536463948@qq.com>
2021-06-28 22:28:34 +0800

    使用代码生成器搭建环境

---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.atguigu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author ChengLongsheng
+ * @since 2021-06-28
+ */
+@RestController
+@RequestMapping("/eduservice/teacher")
+public class EduTeacherController {
+
+}
+

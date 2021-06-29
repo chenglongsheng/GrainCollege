44de03bda4217d859d5396e3a189f1aa4a43dc8e
cls688 <1536463948@qq.com>
2021-06-29 12:23:47 +0800

    配置逻辑删除插件

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/config/EduConfig.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/config/EduConfig.java
@@ -1,6 +1,9 @@
 package com.atguigu.eduservice.config;
 
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,4 +13,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.atguigu.eduservice.mapper")
 public class EduConfig {
+
+    /**
+     * 逻辑删除插件
+     */
+    @Bean
+    public ISqlInjector iSqlInjector() {
+        return new LogicSqlInjector();
+    }
+
 }

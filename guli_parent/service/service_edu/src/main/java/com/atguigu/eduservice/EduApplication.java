1dcf456fe4063a823c4fa126ef3ad44b0b6ee136
cls688 <1536463948@qq.com>
2021-06-29 18:09:51 +0800

    设置包的扫描规则

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -2,12 +2,14 @@ package com.atguigu.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author chenglongsheng
  * @create 2021-06-28 22:41
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.atguigu"})
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);

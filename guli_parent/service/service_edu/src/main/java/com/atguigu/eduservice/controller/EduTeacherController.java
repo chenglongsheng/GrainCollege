ed495153d3cf2f6d01b241759daefa268dd6f6d3
cls688 <1536463948@qq.com>
2021-06-29 18:32:49 +0800

    添加解释注解

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,9 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,7 @@ import java.util.List;
  * @author ChengLongsheng
  * @since 2021-06-28
  */
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
@@ -26,6 +30,7 @@ public class EduTeacherController {
 
     //1.查询讲师表中所有数据
     //rest风格
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("/findAll")
     public List<EduTeacher> findAllTeacher() {
         List<EduTeacher> list = teacherService.list(null);
@@ -33,8 +38,9 @@ public class EduTeacherController {
     }
 
     //2.逻辑删除讲师方法
+    @ApiOperation(value = "逻辑删除讲师")
     @DeleteMapping("{id}")//通过路径传值
-    public boolean remove(@PathVariable String id) {//@PathVariable String id 获取路径中的id
+    public boolean remove(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {//@PathVariable String id 获取路径中的id
         boolean flag = teacherService.removeById(id);
         return flag;
     }

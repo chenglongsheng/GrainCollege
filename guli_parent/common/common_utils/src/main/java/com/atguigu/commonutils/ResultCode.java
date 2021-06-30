acabd126847b672fbbd471b1dd9d9f7d429abb11
cls688 <1536463948@qq.com>
2021-06-30 09:44:14 +0800

    返回码

---
+++ guli_parent/common/common_utils/src/main/java/com/atguigu/commonutils/ResultCode.java
@@ -0,0 +1,12 @@
+package com.atguigu.commonutils;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-06-30 9:23
+ */
+public interface ResultCode {
+
+    public static Integer SUCCESS = 20000;
+    public static Integer ERROR = 20001;
+
+}

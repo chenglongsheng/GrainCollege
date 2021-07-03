72528a8b340f2c7f8358dacabca463cf595d7a9a
cls688 <1536463948@qq.com>
2021-07-03 21:52:31 +0800

    类上添加注解

--- guli_parent/common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ guli_parent/common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.atguigu.servicebase.exceptionhandler;
 
 import com.atguigu.commonutils.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @create 2021-07-02 21:55
  */
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     // 全局异常处理

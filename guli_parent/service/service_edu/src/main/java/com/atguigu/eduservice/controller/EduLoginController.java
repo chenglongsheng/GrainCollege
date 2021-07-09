e68e30f212be30b30fcd34068bbfe113619982fa
cls688 <1536463948@qq.com>
2021-07-09 22:33:37 +0800

    开发login接口

---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduLoginController.java
@@ -0,0 +1,29 @@
+package com.atguigu.eduservice.controller;
+
+import com.atguigu.commonutils.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-07-09 22:11
+ */
+@RestController
+@RequestMapping("/eduservice/user")
+public class EduLoginController {
+
+    // login
+    @PostMapping("/login")
+    public Result login() {
+        return Result.ok().data("token", "admin");
+    }
+
+    // info
+    @GetMapping("/info")
+    public Result info() {
+        return Result.ok().data("roles", "[admin]").data("name", "admin").data("avatar", "http://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+
+}

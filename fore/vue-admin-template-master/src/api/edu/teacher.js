7e8355845e854d76597f0aaca141ea1bf5edf926
cls688 <1536463948@qq.com>
2021-07-11 23:01:30 +0800

    3.定义接口地址和参数

---
+++ fore/vue-admin-template-master/src/api/edu/teacher.js
@@ -0,0 +1,15 @@
+import request from '@/utils/request'
+
+export default {
+    // 条件查询分页
+    // current
+    getTeacherListPage(current, limit, teacherQuery) {
+        return request({
+            // url: '/eduservice/teacher/pageTeacherCondition/' + current + "/" + limit,//方式一
+            url: `/eduservice/teacher/pageTeacherCondition/${current}/${limit}`,//方式二
+            method: 'post',
+            //后端使用ResponseBody 使用json传递数据，把json数据封装到对应对象里面
+            data: teacherQuery
+        })
+    }
+}

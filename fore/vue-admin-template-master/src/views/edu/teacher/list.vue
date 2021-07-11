79900a24cb040d2c3dc2c8e35228db7ad14b9a79
cls688 <1536463948@qq.com>
2021-07-11 23:00:46 +0800

    2.创建对应的页面

---
+++ fore/vue-admin-template-master/src/views/edu/teacher/list.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="list">
+    list
+  </div>
+</template>
+
+<script>
+import teacher from '@/api/edu/teacher'
+export default {
+    data(){
+        return{
+            list:null,//查询之后接口返回集合
+            page:1,//当前页
+            limit:10,//每页记录数
+            total:0,//总记录数
+            teacherQuery:{}//条件封装对象
+        }
+    },
+    created(){
+        this.getList()
+    },
+    methods:{
+        //讲师列表
+        getList() {
+            teacher.getTeacherListPage(this.page, this.limit, this.teacherQuery)
+            .then(response=>{
+                // console.log(response)
+                this.list = response.data.rows
+                this.total = response.data.total
+                console.log(this.list)
+                console.log(this.total)
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ fore/vue-admin-template-master/src/views/edu/teacher/save.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="save">
+      save
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

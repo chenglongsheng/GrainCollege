6c0312b0814d8a930192163a1a0a02a5b140c63d
cls688 <1536463948@qq.com>
2021-07-12 22:45:49 +0800

    表格渲染

--- fore/vue-admin-template-master/src/views/edu/teacher/list.vue
+++ fore/vue-admin-template-master/src/views/edu/teacher/list.vue
@@ -1,43 +1,82 @@
 <template>
-  <div id="list">
-    list
+  <div id="app-container">
+    <!-- 表格 -->
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="数据加载中"
+      border
+      fit
+      highlight-current-row>
+
+      <el-table-column
+        label="序号"
+        width="70"
+        align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="name" label="名称" width="80" />
+
+      <el-table-column label="头衔" width="80">
+        <template slot-scope="scope">
+          {{ scope.row.level===1?'高级讲师':'首席讲师' }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="intro" label="资历" />
+
+      <el-table-column prop="gmtCreate" label="添加时间" width="160"/>
+
+      <el-table-column prop="sort" label="排序" width="60" />
+
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/edu/teacher/edit/'+scope.row.id">
+            <el-button type="primary" size="mini" icon="el-icon-edit">修改</el-button>
+          </router-link>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 import teacher from '@/api/edu/teacher'
 export default {
-    data(){
-        return{
-            list:null,//查询之后接口返回集合
-            page:1,//当前页
-            limit:10,//每页记录数
-            total:0,//总记录数
-            teacherQuery:{}//条件封装对象
-        }
-    },
-    created(){
-        this.getList()
-    },
-    methods:{
-        //讲师列表
-        getList() {
-            teacher.getTeacherListPage(this.page, this.limit, this.teacherQuery)
-            .then(response=>{
-                // console.log(response)
-                this.list = response.data.rows
-                this.total = response.data.total
-                console.log(this.list)
-                console.log(this.total)
-            })
-            .catch(error=>{
-                console.log(error)
-            })
-        }
+  data() {
+    return {
+      list: null,//查询之后接口返回集合
+      page: 1,//当前页
+      limit: 10,//每页记录数
+      total: 0,//总记录数
+      teacherQuery: {},//条件封装对象
     }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    //讲师列表
+    getList() {
+      teacher.getTeacherListPage(this.page, this.limit, this.teacherQuery)
+        .then(response => {
+          // console.log(response)
+          this.list = response.data.rows
+          this.total = response.data.total
+          console.log(this.list)
+          console.log(this.total)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>

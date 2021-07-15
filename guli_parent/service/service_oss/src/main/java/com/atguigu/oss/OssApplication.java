339edfa65342f2b259cc61d58d495f9d50c7b70c
cls688 <1536463948@qq.com>
2021-07-15 11:31:06 +0800

    取消数据源自动配置

--- guli_parent/service/service_oss/src/main/java/com/atguigu/oss/OssApplication.java
+++ guli_parent/service/service_oss/src/main/java/com/atguigu/oss/OssApplication.java
@@ -2,13 +2,14 @@ package com.atguigu.oss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author chenglongsheng
  * @create 2021-07-15 10:58
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan(basePackages = {"com.atguigu"})
 public class OssApplication {
     public static void main(String[] args) {

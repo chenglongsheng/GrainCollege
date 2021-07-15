d2f526e9742a206ea52a50b0f771673e2a36736f
cls688 <1536463948@qq.com>
2021-07-15 11:27:54 +0800

    搭建阿里云oss环境

---
+++ guli_parent/service/service_oss/src/main/java/com/atguigu/oss/OssApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-07-15 10:58
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.atguigu"})
+public class OssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OssApplication.class, args);
+    }
+}

6a00374f3497842925913727a71e60804af5ee3f
cls688 <1536463948@qq.com>
2021-07-16 19:53:57 +0800

    引入讲师头像上传模组

--- fore/vue-admin-template-master/src/views/edu/teacher/save.vue
+++ fore/vue-admin-template-master/src/views/edu/teacher/save.vue
@@ -29,6 +29,36 @@
       </el-form-item>
 
       <!-- 讲师头像：TODO -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar" />
+        <!-- 文件上传按钮 -->
+        <el-button
+          type="primary"
+          icon="el-icon-upload"
+          @click="imagecropperShow = true"
+          >更换头像
+        </el-button>
+
+        <!--
+          v-show：是否显示上传组件
+          :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+          :url：后台上传的url地址
+          @close：关闭上传组件
+          @crop-upload-success：上传成功后的回调 
+        -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="BASE_API + '/admin/oss/file/upload'"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"
+        />
+      </el-form-item>
 
       <el-form-item>
         <el-button
@@ -44,8 +74,11 @@
 
 <script>
 import teacherAPI from '@/api/edu/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
 
 export default {
+  components: { ImageCropper, PanThumb },
   data() {
     return {
       teacher: {
@@ -56,6 +89,9 @@ export default {
         intro: '',
         avatar: ''
       },
+      imagecropperShow: false,// 上传弹框组件是否显示
+      imagecropperKey: 0,// 上传组件key值
+      BASE_API: process.env.BASE_API,// 获取dev.env.js里面的地址
       saveBtnDisabled: false // 保存按钮是否禁用,
     }
   },
@@ -72,6 +108,14 @@ export default {
   },
 
   methods: {
+    // 关闭上传弹框
+    colse() {
+
+    },
+    // 上传成功的方法
+    cropSuccess() {
+
+    },
 
     extract() {
       // 判断路径中有没有id值

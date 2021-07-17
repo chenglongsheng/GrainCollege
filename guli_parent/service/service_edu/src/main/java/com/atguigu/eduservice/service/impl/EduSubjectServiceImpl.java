5412c4fecd23ec94c9014987f4aa0889aae48e20
cls688 <1536463948@qq.com>
2021-07-17 13:25:38 +0800

    实现课程分类接口方法

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
@@ -1,10 +1,16 @@
 package com.atguigu.eduservice.service.impl;
 
+import com.alibaba.excel.EasyExcel;
 import com.atguigu.eduservice.entity.EduSubject;
+import com.atguigu.eduservice.entity.excel.SubjectData;
+import com.atguigu.eduservice.listener.SubjectExcelListener;
 import com.atguigu.eduservice.mapper.EduSubjectMapper;
 import com.atguigu.eduservice.service.EduSubjectService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
 
 /**
  * <p>
@@ -17,4 +23,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubject> implements EduSubjectService {
 
+    // 添加课程分类
+    @Override
+    public void saveSubject(MultipartFile file, EduSubjectService subjectService) {
+        try {
+            // 文件输入流
+            InputStream is = file.getInputStream();
+            // 调用方法读取
+            EasyExcel.read(is, SubjectData.class, new SubjectExcelListener(subjectService)).sheet().doRead();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

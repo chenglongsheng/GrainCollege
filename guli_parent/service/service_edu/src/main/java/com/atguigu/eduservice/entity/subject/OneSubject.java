8ec8044041edf9e12f0fc7437e455b1dbd4aab2d
cls688 <1536463948@qq.com>
2021-07-17 22:21:32 +0800

    第一步：针对返回数据创建对应实体类

---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/entity/subject/OneSubject.java
@@ -0,0 +1,13 @@
+package com.atguigu.eduservice.entity.subject;
+
+import lombok.Data;
+
+/**一级分类
+ * @author chenglongsheng
+ * @create 2021-07-17 22:18
+ */
+@Data
+public class OneSubject {
+    private String id;
+    private String title;
+}
---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/entity/subject/TwoSubject.java
@@ -0,0 +1,13 @@
+package com.atguigu.eduservice.entity.subject;
+
+import lombok.Data;
+
+/**二级分类
+ * @author chenglongsheng
+ * @create 2021-07-17 22:19
+ */
+@Data
+public class TwoSubject {
+    private String id;
+    private String title;
+}

09b928ce35e07bc0db6567349d07fde99398a881
cls688 <1536463948@qq.com>
2021-07-17 11:36:27 +0800

    代码生成器创建课程分类块

---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -0,0 +1,21 @@
+package com.atguigu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 课程科目 前端控制器
+ * </p>
+ *
+ * @author ChengLongsheng
+ * @since 2021-07-17
+ */
+@RestController
+@RequestMapping("/eduservice/subject")
+public class EduSubjectController {
+
+}
+

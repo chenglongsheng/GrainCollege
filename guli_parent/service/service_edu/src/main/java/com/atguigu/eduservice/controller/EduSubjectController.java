be60a3b0f1fed906c7a0bc1122d252d9b0c96996
cls688 <1536463948@qq.com>
2021-07-17 13:23:04 +0800

    课程分类控制

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -1,9 +1,14 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.commonutils.Result;
+import com.atguigu.eduservice.service.EduSubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * <p>
@@ -15,7 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/eduservice/subject")
+@CrossOrigin
 public class EduSubjectController {
+
+    @Autowired
+    private EduSubjectService subjectService;
+
+    // 添加课程分类
+    // 获取上传的文件并读取
+    @PostMapping("/addSubject")
+    public Result addSubject(MultipartFile file) {
+        subjectService.saveSubject(file, subjectService);
+        return Result.ok();
+    }
 
 }
 

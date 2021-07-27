bb838653501a89c1d8f280daebd4327829bacaf7
cls688 <1536463948@qq.com>
2021-07-27 20:54:58 +0800

    添加创建课程大纲样式

--- fore/vue-admin-template-master/src/views/edu/course/chapter.vue
+++ fore/vue-admin-template-master/src/views/edu/course/chapter.vue
@@ -13,25 +13,29 @@
       <el-step title="提交审核" />
     </el-steps>
 
-    <ul>
+    <!-- 章节 -->
+    <ul class="chanpterList">
       <li v-for="chapter in allChapterVideo" :key="chapter.id">
-        {{ chapter.title }}
-        <ul>
+        <p>
+          {{ chapter.title }}
+        </p>
+
+        <!-- 视频 -->
+        <ul class="chanpterList videoList">
           <li v-for="video in chapter.children" :key="video.id">
-            {{ video.title }}
+            <p>
+              {{ video.title }}
+            </p>
           </li>
         </ul>
       </li>
     </ul>
-
-    <el-form label-width="120px">
-      <el-form-item>
-        <el-button @click="previous">上一步</el-button>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="next"
-          >下一步</el-button
-        >
-      </el-form-item>
-    </el-form>
+    <div>
+      <el-button @click="previous">上一步</el-button>
+      <el-button :disabled="saveBtnDisabled" type="primary" @click="next"
+        >下一步</el-button
+      >
+    </div>
   </div>
 </template>
 
@@ -75,3 +79,43 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.chanpterList {
+  position: relative;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.chanpterList li {
+  position: relative;
+}
+.chanpterList p {
+  float: left;
+  font-size: 20px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 70px;
+  line-height: 50px;
+  width: 100%;
+  border: 1px solid #ddd;
+}
+.chanpterList .acts {
+  float: right;
+  font-size: 14px;
+}
+
+.videoList {
+  padding-left: 50px;
+}
+.videoList p {
+  float: left;
+  font-size: 14px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 50px;
+  line-height: 30px;
+  width: 100%;
+  border: 1px dotted #ddd;
+}
+</style>

2dadb2f4f992adcd928d385328192158565fd0c5
cls688 <1536463948@qq.com>
2021-07-29 21:31:02 +0800

    小节控制器

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
@@ -1,9 +1,11 @@
 package com.atguigu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.commonutils.Result;
+import com.atguigu.eduservice.entity.EduVideo;
+import com.atguigu.eduservice.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,7 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/eduservice/video")
+@CrossOrigin
 public class EduVideoController {
+
+    @Autowired
+    private EduVideoService videoService;
+
+    // 添加小节
+    @PostMapping("/addVideo")
+    public Result addVideo(@RequestBody EduVideo eduVideo) {
+        videoService.save(eduVideo);
+        return Result.ok();
+    }
+
+    // 删除小节
+    @DeleteMapping("{id}")
+    public Result deleteVideo(@PathVariable String id) {
+        videoService.removeById(id);
+        return Result.ok();
+    }
+
+    // 修改小节
+    @PostMapping("/updateVideo")
+    public Result updateVideo(@RequestBody EduVideo eduVideo) {
+        videoService.updateById(eduVideo);
+        return Result.ok();
+    }
 
 }
 

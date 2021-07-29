71c00c78c2d93bd836d6551378585ee94796b5d4
cls688 <1536463948@qq.com>
2021-07-29 22:10:13 +0800

    修正名称

---
+++ fore/vue-admin-template-master/src/api/edu/video.js
@@ -0,0 +1,26 @@
+import request from '@/utils/request'
+export default {
+    // 添加小节
+    addVideo(video) {
+        return request({
+            url: `/eduservice/video/addVideo`,
+            method: 'post',
+            data: video
+        })
+    },
+    // 修改小节
+    updateVideo(video) {
+        return request({
+            url: `/eduservice/video/updateVideo`,
+            method: 'post',
+            data: video
+        })
+    },
+    //  删除小节
+    deleteVideo(id) {
+        return request({
+            url: '/eduservice/video/' + id,
+            method: 'delete'
+        })
+    }
+}

18127925b62fd299eae74f9fc8d6b90b715ab9aa
cls688 <1536463948@qq.com>
2021-07-29 10:02:09 +0800

    添加查询修改删除章节

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
@@ -2,6 +2,7 @@ package com.atguigu.eduservice.controller;
 
 
 import com.atguigu.commonutils.Result;
+import com.atguigu.eduservice.entity.EduChapter;
 import com.atguigu.eduservice.entity.chapter.ChapterVo;
 import com.atguigu.eduservice.service.EduChapterService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,39 @@ public class EduChapterController {
     public Result getChapterVideo(@PathVariable String courseId) {
         List<ChapterVo> list = chapterService.getAllChapterVideoByCourseId(courseId);
         return Result.ok().data("allChapterVideo", list);
+    }
+
+    // 添加章节
+    @PostMapping("/addChapter")
+    public Result addChapter(@RequestBody EduChapter eduChapter) {
+        chapterService.save(eduChapter);
+        return Result.ok();
+    }
+
+    // 查询章节
+    @GetMapping("/getChapter/{chapterId}")
+    public Result getChapter(@PathVariable String chapterId) {
+        EduChapter eduChapter = chapterService.getById(chapterId);
+        return Result.ok().data("chapter", eduChapter);
+    }
+
+    // 修改章节
+    @PostMapping("/updateChapter")
+    public Result updateChapter(@RequestBody EduChapter eduChapter) {
+        chapterService.updateById(eduChapter);
+        return Result.ok();
+    }
+
+    // 删除章节
+    @DeleteMapping("/{chapterId}")
+    public Result deleteChapter(@PathVariable String chapterId) {
+        boolean flag = chapterService.deleteChapter(chapterId);
+        if (flag) {
+            return Result.ok();
+        } else {
+            return Result.error();
+        }
+
     }
 
 }

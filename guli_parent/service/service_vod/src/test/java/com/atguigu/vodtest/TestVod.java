a975705701cee2ac1a5f47b56e40128aac9b2b7c
cls688 <1536463948@qq.com>
2021-08-13 21:31:21 +0800

    测试上传视频

--- guli_parent/service/service_vod/src/test/java/com/atguigu/vodtest/TestVod.java
+++ guli_parent/service/service_vod/src/test/java/com/atguigu/vodtest/TestVod.java
@@ -1,7 +1,13 @@
 package com.atguigu.vodtest;
 
+import com.aliyun.vod.upload.impl.UploadVideoImpl;
+import com.aliyun.vod.upload.req.UploadVideoRequest;
+import com.aliyun.vod.upload.resp.UploadVideoResponse;
 import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.vod.model.v20170321.*;
+import com.aliyuncs.vod.model.v20170321.GetPlayInfoRequest;
+import com.aliyuncs.vod.model.v20170321.GetPlayInfoResponse;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
 
 import java.util.List;
 
@@ -10,8 +16,34 @@ import java.util.List;
  * @create 2021-08-11 22:05
  */
 public class TestVod {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
+        // 上传视频的方法
+        String accessKeyId = "LTAI5t7zEyNP7cPU7tbaiUrm";
+        String accessKeySecret = "Sw1YLSKSHUdD14dyPvfBKaCDvoMByU";
+        String title = "6 - What If I Want to Move Faster - upload by sdk";
+        String fileName = "F:\\guli_edu\\6 - What If I Want to Move Faster.mp4";
+        UploadVideoRequest request = new UploadVideoRequest(accessKeyId, accessKeySecret, title, fileName);
+        /* 可指定分片上传时每个分片的大小，默认为2M字节 */
+        request.setPartSize(2 * 1024 * 1024L);
+        /* 可指定分片上传时的并发线程数，默认为1，(注：该配置会占用服务器CPU资源，需根据服务器情况指定）*/
+        request.setTaskNum(1);
+        UploadVideoImpl uploader = new UploadVideoImpl();
+        UploadVideoResponse response = uploader.uploadVideo(request);
+        System.out.print("RequestId=" + response.getRequestId() + "\n");  //请求视频点播服务的请求ID
+        if (response.isSuccess()) {
+            System.out.print("VideoId=" + response.getVideoId() + "\n");
+        } else {
+            /* 如果设置回调URL无效，不影响视频上传，可以返回VideoId同时会返回错误码。其他情况上传失败时，VideoId为空，此时需要根据返回错误码分析具体错误原因 */
+            System.out.print("VideoId=" + response.getVideoId() + "\n");
+            System.out.print("ErrorCode=" + response.getCode() + "\n");
+            System.out.print("ErrorMessage=" + response.getMessage() + "\n");
+        }
+
+
+    }
+
+    public static void getVideoAuth() throws Exception {
         // 根据视频id获取视频播放凭证
         // 创建初始化对象
         DefaultAcsClient client = InitObject.initVodClient("LTAI5t7zEyNP7cPU7tbaiUrm", "Sw1YLSKSHUdD14dyPvfBKaCDvoMByU");

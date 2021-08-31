c8c51b906203835847a7ead48199528609691b3c
cls688 <1536463948@qq.com>
2021-08-31 19:48:19 +0800

    删除多个阿里云视频 参数有多个视频id

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/client/VodClient.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/client/VodClient.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * @author chenglongsheng
@@ -18,5 +21,9 @@ public interface VodClient {
     // 根据视频id删除阿里云视频
     @DeleteMapping(value = "/eduvod/video/deleteAliyunVideo/{id}")
     public Result deleteAliyunVideo(@PathVariable("id") String id);
+
+    // 删除多个阿里云视频 参数有多个视频id
+    @DeleteMapping("/eduvod/video/delete-batch")
+    public Result deleteBatch(@RequestParam("videoIdList") List<String> videoIdList);
 
 }
--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduVideoServiceImpl.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduVideoServiceImpl.java
@@ -1,11 +1,17 @@
 package com.atguigu.eduservice.service.impl;
 
+import com.atguigu.eduservice.client.VodClient;
 import com.atguigu.eduservice.entity.EduVideo;
 import com.atguigu.eduservice.mapper.EduVideoMapper;
 import com.atguigu.eduservice.service.EduVideoService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -18,9 +24,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class EduVideoServiceImpl extends ServiceImpl<EduVideoMapper, EduVideo> implements EduVideoService {
 
+    @Autowired
+    private VodClient vodClient;
+
     //1 根据课程id删除小节
     @Override
     public void removeVideoByCourseId(String courseId) {
+
+        //删除小节中的视频
+        QueryWrapper<EduVideo> wrapperVideo = new QueryWrapper<>();
+        wrapperVideo.eq("course_id", courseId);
+        wrapperVideo.select("video_source_id");
+        List<EduVideo> videoList = baseMapper.selectList(wrapperVideo);
+
+        List<String> videoIds = new ArrayList<>();
+        for (EduVideo eduVideo : videoList) {
+            String videoSourceId = eduVideo.getVideoSourceId();
+            if (!StringUtils.isEmpty(videoSourceId)) {
+                videoIds.add(videoSourceId);
+            }
+        }
+
+
+        // 根据视频id集合删除多个视频
+        if (videoIds.size() > 0) {
+            vodClient.deleteBatch(videoIds);
+        }
+
+
         QueryWrapper<EduVideo> wrapper = new QueryWrapper<>();
         wrapper.eq("course_id", courseId);
         baseMapper.delete(wrapper);

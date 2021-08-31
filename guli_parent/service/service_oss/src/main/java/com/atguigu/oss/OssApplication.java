66f9ba36a0ed11d20822b7698d6600b6dd110e73
cls688 <1536463948@qq.com>
2021-08-31 16:31:57 +0800

    引入服务调用

--- guli_parent/service/service_oss/src/main/java/com/atguigu/oss/OssApplication.java
+++ guli_parent/service/service_oss/src/main/java/com/atguigu/oss/OssApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.oss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @create 2021-07-15 10:58
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.atguigu"})
 public class OssApplication {
     public static void main(String[] args) {

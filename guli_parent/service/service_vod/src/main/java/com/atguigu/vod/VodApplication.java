73e5a2147a5c60a6adabe567befe6a4030c3e114
cls688 <1536463948@qq.com>
2021-08-31 09:07:23 +0800

    在nacos中注册vod

--- guli_parent/service/service_vod/src/main/java/com/atguigu/vod/VodApplication.java
+++ guli_parent/service/service_vod/src/main/java/com/atguigu/vod/VodApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.vod;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @create 2021-08-14 21:32
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.atguigu"})
 public class VodApplication {
     public static void main(String[] args) {

29c483568f14f30cbac1fcac8ffdd58d5861fbcd
cls688 <1536463948@qq.com>
2021-09-01 21:08:07 +0800

    cms模块

---
+++ guli_parent/service/service_cms/src/main/java/com/atguigu/cmsservice/controller/CrmBannerController.java
@@ -0,0 +1,21 @@
+package com.atguigu.cmsservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 首页banner表 前端控制器
+ * </p>
+ *
+ * @author ChengLongsheng
+ * @since 2021-09-01
+ */
+@RestController
+@RequestMapping("/cmsservice/crm-banner")
+public class CrmBannerController {
+
+}
+

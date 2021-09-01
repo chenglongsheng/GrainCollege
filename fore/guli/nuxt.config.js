6d849b3db70a8eddd320ac63326ed7670344873f
cls688 <1536463948@qq.com>
2021-09-01 17:27:06 +0800

    依赖和配置

--- fore/guli/nuxt.config.js
+++ fore/guli/nuxt.config.js
@@ -24,16 +24,30 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
+          options: {
+            fix: true,
+          }
         })
       }
     }
-  }
+  },
+  plugins: [
+
+    { src: '~/plugins/nuxt-swiper-plugin.js', ssr: false }
+
+  ],
+
+  css: [
+
+    'swiper/dist/css/swiper.css'
+
+  ]
 }
 

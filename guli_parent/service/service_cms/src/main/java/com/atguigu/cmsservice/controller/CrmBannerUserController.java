f1c0fb636a4809a5ca978bfc6fa04365407a2f16
cls688 <1536463948@qq.com>
2021-09-02 20:10:55 +0800

    跨域

--- guli_parent/service/service_cms/src/main/java/com/atguigu/cmsservice/controller/CrmBannerUserController.java
+++ guli_parent/service/service_cms/src/main/java/com/atguigu/cmsservice/controller/CrmBannerUserController.java
@@ -4,6 +4,7 @@ import com.atguigu.cmsservice.entity.CrmBanner;
 import com.atguigu.cmsservice.service.CrmBannerService;
 import com.atguigu.commonutils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/cmsservice/bannerUser")
+@CrossOrigin
 public class CrmBannerUserController {
 
     @Autowired

0f4959ffb6d6b1178ca72a18fe63fb10f3b0347d
cls688 <1536463948@qq.com>
2021-09-02 15:11:21 +0800

    nacos注册

--- guli_parent/service/service_cms/src/main/java/com/atguigu/cmsservice/CmsApplication.java
+++ guli_parent/service/service_cms/src/main/java/com/atguigu/cmsservice/CmsApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.cmsservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan({"com.atguigu"}) //指定扫描位置
+@EnableDiscoveryClient //nacos注册
 @MapperScan("com.atguigu.cmsservice.mapper")
 public class CmsApplication {
     public static void main(String[] args) {

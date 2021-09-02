22d3200fc85d4d7f6ea6264e3be8d3b59ae8081e
cls688 <1536463948@qq.com>
2021-09-02 20:10:41 +0800

    查询所有banner

--- guli_parent/service/service_cms/src/main/java/com/atguigu/cmsservice/service/impl/CrmBannerServiceImpl.java
+++ guli_parent/service/service_cms/src/main/java/com/atguigu/cmsservice/service/impl/CrmBannerServiceImpl.java
@@ -3,6 +3,7 @@ package com.atguigu.cmsservice.service.impl;
 import com.atguigu.cmsservice.entity.CrmBanner;
 import com.atguigu.cmsservice.mapper.CrmBannerMapper;
 import com.atguigu.cmsservice.service.CrmBannerService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +23,13 @@ public class CrmBannerServiceImpl extends ServiceImpl<CrmBannerMapper, CrmBanner
     //查询所有banner
     @Override
     public List<CrmBanner> selectAllBanner() {
+
+        //根据id进行降序排列,显示排序后的前两条记录
+        QueryWrapper<CrmBanner> wrapper = new QueryWrapper<>();
+        wrapper.orderByDesc("id");
+        //last方法，拼接sql语句
+        wrapper.last("limit 2");
         return baseMapper.selectList(null);
+
     }
 }

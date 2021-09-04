664213eb8f05cf3c575b22ee1b513d838fa86d65
cls688 <1536463948@qq.com>
2021-09-04 20:17:32 +0800

    查询热门课程和讲师 接口路径

---
+++ fore/guli/api/index.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+
+export default {
+  // 查询热门课程和讲师
+  getIndex() {
+    return request({
+      url: '/eduservice/indexfront/index',
+      method: 'get'
+    })
+  }
+}

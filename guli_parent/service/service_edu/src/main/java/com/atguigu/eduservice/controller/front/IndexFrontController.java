d9414dda1f262f0e733e235ccc76741731355311
cls688 <1536463948@qq.com>
2021-09-04 09:47:43 +0800

    查询前八条热门课程和前四条名师 接口

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/IndexFrontController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/IndexFrontController.java
@@ -2,10 +2,12 @@ package com.atguigu.eduservice.controller.front;
 
 import com.atguigu.commonutils.Result;
 import com.atguigu.eduservice.entity.EduCourse;
+import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduCourseService;
 import com.atguigu.eduservice.service.EduTeacherService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/eduservice/indexfront")
+@CrossOrigin
 public class IndexFrontController {
 
     @Autowired
@@ -30,13 +33,18 @@ public class IndexFrontController {
     @GetMapping("/index")
     public Result index() {
         //查询前八条课程
-        QueryWrapper<EduCourse> wrapper = new QueryWrapper<>();
-        wrapper.orderByDesc("id");
-        wrapper.last("limit 8");// 拼接字符
-        List<EduCourse> list = courseService.list(wrapper);
+        QueryWrapper<EduCourse> courseWrapper = new QueryWrapper<>();
+        courseWrapper.orderByDesc("id");
+        courseWrapper.last("limit 8");// 拼接字符
+        List<EduCourse> courseList = courseService.list(courseWrapper);
 
+        //查询前四条名师
+        QueryWrapper<EduTeacher> teacherWrapper = new QueryWrapper<>();
+        teacherWrapper.orderByDesc("id");
+        teacherWrapper.last("limit 4");// 拼接字符
+        List<EduTeacher> teacherList = teacherService.list(teacherWrapper);
 
-        return Result.ok();
+        return Result.ok().data("courseList", courseList).data("teacherList", teacherList);
     }
 
 }

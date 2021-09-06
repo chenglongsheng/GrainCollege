585aae17f049e3c960995911e090130e86013cb2
cls688 <1536463948@qq.com>
2021-09-06 21:09:30 +0800

    短信服务前端控制器

---
+++ guli_parent/service/service_msm/src/main/java/com/atguigu/msmservice/controller/MsmController.java
@@ -0,0 +1,15 @@
+package com.atguigu.msmservice.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-09-06 20:18
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/Edumsm/msm")
+public class MsmController {
+}

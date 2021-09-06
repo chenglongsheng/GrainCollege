0c02ff42f115e982b477dc2e7111d076405a1472
cls688 <1536463948@qq.com>
2021-09-06 21:09:42 +0800

    短信服务后端服务

---
+++ guli_parent/service/service_msm/src/main/java/com/atguigu/msmservice/service/Impl/MsmServiceImpl.java
@@ -0,0 +1,12 @@
+package com.atguigu.msmservice.service.Impl;
+
+import com.atguigu.msmservice.service.MsmService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-09-06 20:20
+ */
+@Service
+public class MsmServiceImpl implements MsmService {
+}
---
+++ guli_parent/service/service_msm/src/main/java/com/atguigu/msmservice/service/MsmService.java
@@ -0,0 +1,8 @@
+package com.atguigu.msmservice.service;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-09-06 20:20
+ */
+public interface MsmService {
+}

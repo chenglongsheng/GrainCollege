33a520180a8aae916fa4fdc1a0c007ab8464a286
cls688 <1536463948@qq.com>
2021-09-07 16:13:30 +0800

    nacos注册

--- guli_parent/service/service_msm/src/main/java/com/atguigu/msmservice/MsmApplication.java
+++ guli_parent/service/service_msm/src/main/java/com/atguigu/msmservice/MsmApplication.java
@@ -3,12 +3,14 @@ package com.atguigu.msmservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author chenglongsheng
  * @create 2021-09-06 20:14
  */
+@EnableDiscoveryClient
 @ComponentScan({"com.atguigu"})
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动配置
 public class MsmApplication {

70d2f190212e24acad52f5607edb34d4810ab027
cls688 <1536463948@qq.com>
2021-09-09 19:27:58 +0800

    添加插件

--- fore/guli/plugins/nuxt-swiper-plugin.js
+++ fore/guli/plugins/nuxt-swiper-plugin.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper/dist/ssr'
+import VueQriously from 'vue-qriously'
+import ElementUI from 'element-ui'// element-ui的全部插件
+import 'element-ui/lib/theme-chalk/index.css'// element-ui 的css
 
+Vue.use(VueQriously)
+Vue.use(ElementUI)
 Vue.use(VueAwesomeSwiper)

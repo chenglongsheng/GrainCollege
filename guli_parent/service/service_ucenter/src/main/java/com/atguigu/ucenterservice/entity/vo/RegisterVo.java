c46b2ecd64b442ec730014ccd6aced287ba6c638
cls688 <1536463948@qq.com>
2021-09-09 17:38:21 +0800

    封装注册所需信息

---
+++ guli_parent/service/service_ucenter/src/main/java/com/atguigu/ucenterservice/entity/vo/RegisterVo.java
@@ -0,0 +1,25 @@
+package com.atguigu.ucenterservice.entity.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-09-09 15:54
+ */
+@Data
+public class RegisterVo {
+
+    @ApiModelProperty(value = "昵称")
+    private String nickname;
+
+    @ApiModelProperty(value = "手机号")
+    private String mobile;
+
+    @ApiModelProperty(value = "密码")
+    private String password;
+
+    @ApiModelProperty(value = "验证码")
+    private String code;
+
+}

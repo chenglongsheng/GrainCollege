a68482302770da0f7dbd80248247441bfce5d3dd
cls688 <1536463948@qq.com>
2021-09-09 18:09:07 +0800

    根据token获取用户信息

--- guli_parent/service/service_ucenter/src/main/java/com/atguigu/ucenterservice/controller/UcenterMemberController.java
+++ guli_parent/service/service_ucenter/src/main/java/com/atguigu/ucenterservice/controller/UcenterMemberController.java
@@ -1,12 +1,15 @@
 package com.atguigu.ucenterservice.controller;
 
 
+import com.atguigu.commonutils.JwtUtils;
 import com.atguigu.commonutils.Result;
 import com.atguigu.ucenterservice.entity.UcenterMember;
 import com.atguigu.ucenterservice.entity.vo.RegisterVo;
 import com.atguigu.ucenterservice.service.UcenterMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -38,5 +41,14 @@ public class UcenterMemberController {
         return Result.ok();
     }
 
-}
+    // 根据token获取用户信息
+    @GetMapping("/getMemberInfo")
+    public Result getMemberInfo(HttpServletRequest request) {
+        // 调用JWT工具类的方法，根据request对象获取头信息，返回用户id
+        String memberId = JwtUtils.getMemberIdByJwtToken(request);
+        // 查询数据库，根据用户id获取用户信息
+        UcenterMember member = memberService.getById(memberId);
+        return Result.ok().data("memberInfo", member);
+    }
 
+}

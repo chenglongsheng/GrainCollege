c5fab10f984dee06385f469317bbcad589f22d66
cls688 <1536463948@qq.com>
2021-09-15 10:48:54 +0800

    nacos注册

--- guli_parent/service/service_ucenter/src/main/java/com/atguigu/ucenterservice/ServiceUcApplication.java
+++ guli_parent/service/service_ucenter/src/main/java/com/atguigu/ucenterservice/ServiceUcApplication.java
@@ -3,12 +3,14 @@ package com.atguigu.ucenterservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author chenglongsheng
  * @create 2021-09-07 19:54
  */
+@EnableDiscoveryClient
 @ComponentScan({"com.atguigu"})
 @SpringBootApplication//取消数据源自动配置
 @MapperScan("com.atguigu.ucenterservice.mapper")

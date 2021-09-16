560e45e05c2f6e06fb21a18deddd0794e4f46e8a
cls688 <1536463948@qq.com>
2021-09-16 17:49:41 +0800

    讲师详情功能接口

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/TeacherFrontController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/TeacherFrontController.java
@@ -1,15 +1,16 @@
 package com.atguigu.eduservice.controller.front;
 
 import com.atguigu.commonutils.Result;
+import com.atguigu.eduservice.entity.EduCourse;
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduCourseService;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,6 +24,9 @@ public class TeacherFrontController {
     @Autowired
     private EduTeacherService teacherService;
 
+    @Autowired
+    private EduCourseService courseService;
+
     // 1分页查询讲师
     @PostMapping("/getTeacherList/{page}/{limit}")
     public Result getTeacherList(@PathVariable long page, @PathVariable long limit) {
@@ -31,6 +35,18 @@ public class TeacherFrontController {
         Map<String, Object> map = teacherService.getTeacherFrontList(teacherPage);
 
         return Result.ok().data(map);
+    }
+
+    // 2讲师详情功能
+    @GetMapping("/getTeacherFrontInfo/{teacherId}")
+    public Result getTeacherFrontInfo(@PathVariable String teacherId) {
+        //1 根据讲师id查询讲师基本信息
+        EduTeacher teacher = teacherService.getById(teacherId);
+        //2 根据讲师id查询所有课程
+        QueryWrapper<EduCourse> wrapper = new QueryWrapper<>();
+        wrapper.eq("teacher_id", teacherId);
+        List<EduCourse> courses = courseService.list(wrapper);
+        return Result.ok().data("teacher", teacher).data("courseList", courses);
     }
 
 }

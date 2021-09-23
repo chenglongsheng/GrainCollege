53f62625a178761cf5840ebeded2c153614393e2
cls688 <1536463948@qq.com>
2021-09-23 20:57:04 +0800

    根据课程id，编写SQL语句查询课程信息（mapper）

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/mapper/EduCourseMapper.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/mapper/EduCourseMapper.java
@@ -2,6 +2,7 @@ package com.atguigu.eduservice.mapper;
 
 import com.atguigu.eduservice.entity.EduCourse;
 import com.atguigu.eduservice.entity.vo.CoursePublishVo;
+import com.atguigu.eduservice.entity.vo.front.CourseWebVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
@@ -16,4 +17,6 @@ public interface EduCourseMapper extends BaseMapper<EduCourse> {
 
     public CoursePublishVo getPublicCourseInfo(String courseId);
 
+    // 根据课程id，编写SQL语句查询课程信息
+    CourseWebVo getBaseCourseInfo(String courseId);
 }

24f5d7ab199d60e805f417a6cb8d190327f99849
cls688 <1536463948@qq.com>
2021-09-28 20:13:04 +0800

    可选设置

--- fore/guli/pages/player/_vid.vue
+++ fore/guli/pages/player/_vid.vue
@@ -34,7 +34,18 @@ export default {
       playauth: this.playAuth, // 播放凭证
       encryptType: '1', // 如果播放加密视频，则需设置encryptType=1，非加密视频无需设置此项
       width: '100%',
-      height: '500px'
+      height: '500px',
+      // 以下可选设置
+      cover: 'http://guli.shop/photo/banner/1525939573202.jpg', // 封面
+      qualitySort: 'asc', // 清晰度排序
+
+      mediaType: 'video', // 返回音频还是视频
+      autoplay: false, // 自动播放
+      isLive: false, // 直播
+      rePlay: false, // 循环播放
+      preload: true,
+      controlBarVisibility: 'hover', // 控制条的显示方式：鼠标悬停
+      useH5Prism: true // 播放器类型：html5
     }, function(player) {
       console.log('播放器创建成功')
     })

151eca5794be48b8d25938038cd117a3f78974df
cls688 <1536463948@qq.com>
2021-09-29 20:18:36 +0800

    评论服务

---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/EduCommentService.java
@@ -0,0 +1,12 @@
+package com.atguigu.eduservice.service;
+
+import com.atguigu.eduservice.entity.EduComment;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-09-29 13:30
+ */
+public interface EduCommentService extends IService<EduComment> {
+
+}
---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduCommentServiceImpl.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduservice.service.impl;
+
+import com.atguigu.eduservice.entity.EduComment;
+import com.atguigu.eduservice.mapper.EduCommentMapper;
+import com.atguigu.eduservice.service.EduCommentService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-09-29 13:31
+ */
+@Service
+public class EduCommentServiceImpl extends ServiceImpl<EduCommentMapper, EduComment> implements EduCommentService {
+
+}

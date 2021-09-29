a8f2349d60130181f0013b791215e6c7a3c1bbbd
cls688 <1536463948@qq.com>
2021-09-29 20:21:41 +0800

    评论模块

--- fore/guli/pages/course/_id.vue
+++ fore/guli/pages/course/_id.vue
@@ -158,20 +158,152 @@
       </div>
     </section>
     <!-- /课程详情 结束 -->
+    <div class="mt50 commentHtml"><div>
+      <h6 id="i-art-comment" class="c-c-content c-infor-title">
+        <span class="commentTitle">课程评论</span>
+      </h6>
+      <section class="lh-bj-list pr mt20 replyhtml">
+        <ul>
+          <li class="unBr">
+            <aside class="noter-pic">
+              <img :src="comment.avatar" width="50" height="50" class="picImg">
+            </aside>
+            <div class="of">
+              <section class="n-reply-wrap">
+                <fieldset>
+                  <textarea id="commentContent" v-model="comment.content" name="" placeholder="输入您要评论的文字"/>
+                </fieldset>
+                <p class="of mt5 tar pl10 pr10">
+                  <span class="fl "><tt class="c-red commentContentmeg" style="display: none;"/></span>
+                  <input type="button" value="回复" class="lh-reply-btn" @click="addComment()">
+                </p>
+              </section>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <section class="">
+        <section class="question-list lh-bj-list pr">
+          <ul class="pr10">
+            <li v-for="(comment,index) in data.items" :key="index">
+              <aside class="noter-pic">
+                <img :src="comment.avatar" width="50" height="50" class="picImg">
+              </aside>
+              <div class="of">
+                <span class="fl">
+                  <font class="fsize12 c-blue">
+                    {{ comment.nickname }}</font>
+                <font class="fsize12 c-999 ml5">评论：</font></span>
+              </div>
+              <div class="noter-txt mt5">
+                <p>{{ comment.content }}</p>
+              </div>
+              <div class="of mt5">
+                <span class="fr"><font class="fsize12 c-999 ml5">{{ comment.gmtCreate }}</font></span>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </section>
+
+      <!-- 公共分页 开始 -->
+      <div class="paging">
+        <!-- undisable这个class是否存在，取决于数据属性hasPrevious -->
+        <a
+          :class="{undisable: !data.hasPrevious}"
+          href="#"
+          title="首页"
+          @click.prevent="gotoPage(1)">首</a>
+        <a
+          :class="{undisable: !data.hasPrevious}"
+          href="#"
+          title="前一页"
+          @click.prevent="gotoPage(data.current-1)">&lt;</a>
+        <a
+          v-for="page in data.pages"
+          :key="page"
+          :class="{current: data.current == page, undisable: data.current == page}"
+          :title="'第'+page+'页'"
+          href="#"
+          @click.prevent="gotoPage(page)">{{ page }}</a>
+        <a
+          :class="{undisable: !data.hasNext}"
+          href="#"
+          title="后一页"
+          @click.prevent="gotoPage(data.current+1)">&gt;</a>
+        <a
+          :class="{undisable: !data.hasNext}"
+          href="#"
+          title="末页"
+          @click.prevent="gotoPage(data.pages)">末</a>
+        <div class="clear"/>
+      </div>
+      <!-- 公共分页 结束 -->
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 import courseApi from '@/api/course'
+import commentApi from '@/api/comment'
 
 export default {
   asyncData({ params, error }) {
-    return courseApi.getCourseInfo(params.id).then(response => {
-      return {
-        courseWebVo: response.data.data.courseWebVo,
-        chapterVideoList: response.data.data.chapterVideoList
-      }
-    })
+    return { courseId: params.id }
+  },
+  data() {
+    return {
+      data: {},
+      page: 1,
+      limit: 4,
+      total: 10,
+      comment: {
+        content: '',
+        courseId: '',
+        avatar: ''
+      },
+      courseInfo: {},
+      chapterVideoList: [],
+      isbuyCourse: false,
+      courseWebVo: {}
+    }
+  },
+  created() {
+    this.initCourseInfo()
+    this.initComment()
+  },
+  methods: {
+    initCourseInfo() {
+      courseApi.getCourseInfo(this.courseId).then(response => {
+        this.courseWebVo = response.data.data.courseWebVo
+        this.courseInfo = response.data.data.courseWebVo
+        this.chapterVideoList = response.data.data.chapterVideoList
+        this.isbuyCourse = response.data.data.isbuyCourse
+      })
+    },
+    initComment() {
+      commentApi.getCommentList(this.page, this.limit, this.courseId).then(response => {
+        this.data = response.data.data
+      })
+    },
+    addComment() {
+      this.comment.courseId = this.courseId
+      this.comment.teacherId = this.courseInfo.teacherId
+      commentApi.addComment(this.comment).then(response => {
+        if (response.data.success) {
+          this.comment.content = ''
+          this.comment.avatar = response.data.data.avatar
+          // console.alert(response.data.data.avatar)
+          this.initComment()
+        }
+      })
+    },
+    gotoPage(page) {
+      commentApi.getCommentList(page, this.limit, this.courseId).then(response => {
+        this.data = response.data.data
+      })
+    }
   }
 }
 </script>

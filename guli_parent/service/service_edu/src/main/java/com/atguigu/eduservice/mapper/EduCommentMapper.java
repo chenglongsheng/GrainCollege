b122eca5ee6d92ab3e224df38901b6252796b8e6
cls688 <1536463948@qq.com>
2021-09-29 20:17:41 +0800

    评论mapper

---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/mapper/EduCommentMapper.java
@@ -0,0 +1,11 @@
+package com.atguigu.eduservice.mapper;
+
+import com.atguigu.eduservice.entity.EduComment;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-09-29 13:29
+ */
+public interface EduCommentMapper extends BaseMapper<EduComment> {
+}
